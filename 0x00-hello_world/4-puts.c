e8e18319195385d75436d2d5f5f75ce56a9e5e70
JOSEPH SHOLA PAUL <jsphpaul546@gmail.com>
2023-02-20 00:23:08 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Discription: A programming that prints a line with puts function
+ * Return: (0) (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
