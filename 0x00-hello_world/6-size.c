2cca921bbf784df14a87f140a4d28d589c46dc47
joseph shola paul <jsphpaul546@gmail.com>
2023-05-18 09:52:06 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,20 @@
 #include<stio.h>
 /**
  * main - Entry point
- *
- * Return: Always 0 (Success)
+ * Return 0 (Success)
  */
 int main(void)
 {
-	prinf("Size of a char: %d byte(s)\n", sizeof(char));
-	prinf("Size of an int: %d byte(s)\n", sizeof(int));
-	prinf("Size of a long int: %d byte(s)\n", sizeof(long int));
-	prinf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-	prinf("Size of a float: %d byte(s)\n", sizeof(float));
-	return (0);
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("size of a long int: %lu bytes(s)\n", (unsigned long)sizeof(c));
+printf("size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(d));
+printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+return (0);
 }
-	
