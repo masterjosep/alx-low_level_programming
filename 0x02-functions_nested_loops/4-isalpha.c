5b98178d6aee49b7703ac02f8ec3f14e3ad5cf38
joseph shola paul <jsphpaul546@gmail.com>
2023-05-23 08:55:52 -0700

    A function that checks for alphabetic character

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isalph - Checks for alphabetic character
+ * _isalpha - Checks for alphabetic character
  * @c: The character to be checked
  * Return: 1 for alphabetic character or 0 for anything else
  */
