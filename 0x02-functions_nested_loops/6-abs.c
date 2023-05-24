66bf07cb7df1b7bdf623ac1709954878c1606fc6
joseph shola paul <jsphpaul546@gmail.com>
2023-05-24 01:48:15 -0700

    function that prints the last digit of a number

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,19 +1,20 @@
 #include "main.h"
- /**
-  * _abs - computes the absolute value of an integer.
-  * @c: The number to be computed
-  * Return: Absolute value of number or zero
-  */
+
+/**
+ * _abs - computes the absolute value of integer.
+ * @c: The number to be computed.
+ * Return: Absolute value of number or zero
+ */
 
 int _abs(int c)
 
 {
-	if (c < 0)
-	{
-	int abs_val;
+if (c < 0)
+{
+int abs_val;
 
-	abs_val = c * -1;
-	return (abs_val);
-	}
-	return (c);
+abs_val = c * -1;
+return (abs_val);
+}
+return (c);
 }
