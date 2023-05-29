1acbb86a07d58bbafa48ca958faf2e7276378b2f
joseph shola paul <jsphpaul546@gmail.com>
2023-05-29 03:51:36 -0700

    project prototypes

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,7 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that takes a pointer to an int as parameter and updates the value it points to 98
+ * @n:- The value to be updated to 98
+ * Return: 0
+ */
