5f1d65959aefd7a1870b414b29ce1ef579297ec2
joseph shola paul <jsphpaul546@gmail.com>
2023-05-29 10:22:53 -0700

    A  function that convert a string to an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -21,11 +21,11 @@ int _atoi(char *s)
 	while (i < len && f == 0)
 	{
 		if (s[i] == '-')
-			d++;
+			++d;
 
 		if (s[i] >= '0' && s[i] <= '9')
 		{
-			digit = s[i] - '9';
+			digit = s[i] - '0';
 			if (d % 2)
 				digit = -digit;
 			n = n * 10 + digit;
