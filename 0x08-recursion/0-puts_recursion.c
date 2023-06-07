58b3cf30724b838f0b7da4cbd3cc2ba79e679e8f
joseph shola paul <jsphpaul546@gmail.com>
2023-06-07 08:57:25 -0700

    A  function that prints a string, followed by a new line

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,17 +1,19 @@
 #include "main.h"
 /**
- * _puts_recusion - function like puts ();
+ * _puts_recusion - function like puts();
+ *
  * @s: input
+ *
  * Return: 0 (on track)
- * */
+ */
 void _puts_recursion(char *s)
 {
-	if (*s)
-	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
-	
-	else
-		_putchar('\n');
+if (*s)
+{
+_putchar(*s);
+_puts_recursion(s + 1);
+}
+
+else
+_putchar('\n');
 }
