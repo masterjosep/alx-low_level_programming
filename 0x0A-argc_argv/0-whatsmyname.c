91d3263cd16c28c5302602b481b6803437edb760
joseph shola paul <jsphpaul546@gmail.com>
2023-06-12 01:13:27 -0700

    A  program that prints its name, followed by a new line

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,15 +1,14 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - prints the name of the program
  * @argc: number of arguments
  * @argv: array of arguments
  *
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
-
-int main(int argc _attribute_((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", *argv);
 
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print the number of arguments passed to the program
+ * @argc: number of arguments
+ * @argv: array of argument
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(int argc, char *argv[]                             0
