7ab6800496b98b50093219929aeccef82f5bdc22
joseph shola paul <jsphpaul@gmail.com>
2023-06-23 05:51:43 -0700

    A  function that returns the sum of all its parameters

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,11 +1,13 @@
-variadic_functions.h
+#ifndef VARIADIC_H
+#define VARIADIC_H
 
-#ifndef _VARIADIC_H_
-#define _VARIADIC_H_
+#include <stdio.h>
+#include <stdarg.h>
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
 #endif
